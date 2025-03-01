f3a7c7128ec4d5a59ecbfae484d4269379735f54
Evgeshka23 <143422172+Evgeshka23@users.noreply.github.com>
2025-03-01 22:10:28 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+// Task 1
+// function checkAge(age) {
+//     return (age > 18) ? true : confirm('Батьки дозволили?');
+// }
+
+// Task 2
+
+// function min(a, b) {
+//     return (a < b) ? a : b;
+// }
+
+// Task 3
+
+// function ask (question, yes, no) {
+//     if (confirm(question)) yes()
+//     else no();
+// }
+
+// ask(
+//     "Ви погоджуєтесь?",
+//     () => alert("Ви погодились."),
+//     () => alert("Ви відмовились.")
+// );
